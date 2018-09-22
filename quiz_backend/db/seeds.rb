7bd5f3bd7d51a37058fa4f1d9a2e92d3e8394262
lifeprolific <thegregoman@gmail.com>
2018-09-22 11:01:16 -0400

    added quiz/question/answer seed

--- quiz_backend/db/seeds.rb
+++ quiz_backend/db/seeds.rb
@@ -18,7 +18,35 @@ USERS_TABLE.each {|row|
 }
 
 QUIZZES_TABLE = CSV.read("../samples/quizzes.csv", converters: :numeric, headers: true)
+QUESTIONS_TABLE = CSV.read("../samples/questions.csv", converters: :numeric, headers: true)
+ANSWERS_TABLE = CSV.read("../samples/answers.csv", converters: :numeric, headers: true)
 
-# QUESTIONS_TABLE = CSV.read("../samples/questions.csv", converters: :numeric, headers: true)
+QUIZZES_TABLE.each {|quiz_row|
+  quiz = Quiz.new(
+    title:quiz_row["title"],
+    description:quiz_row["description"],
+  )
+  quiz.save
+  
+  QUESTIONS_TABLE.select {|question_row_search|
+    question_row_search["quiz_id"] == quiz_row["id"]
+    }.each {|question_row_match|
+      question = Question.new(
+        body:question_row_match["body"],
+        quiz_id:quiz.id
+      )
+      question.save
 
-# ANSWERS_TABLE = CSV.read("../samples/answers.csv", converters: :numeric, headers: true)
+      ANSWERS_TABLE.select {|answer_row_search|
+        answer_row_search["question_id"] == question_row_match["id"]
+        }.each {|answer_row_match|
+          answer = Answer.new(
+            body:answer_row_match["body"],
+            correct:answer_row_match["correct"],
+            question_id:question.id
+          )
+          answer.save
+        }
+    }
+
+}

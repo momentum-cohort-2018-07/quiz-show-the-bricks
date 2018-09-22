6bf13383039c50e0dc6bb2fdc8b486469973f999
lifeprolific <thegregoman@gmail.com>
2018-09-22 10:30:00 -0400

    added users seed

--- quiz_backend/db/seeds.rb
+++ quiz_backend/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+USERS_TABLE = CSV.read("../samples/users.csv", converters: :numeric, headers: true)
+USERS_TABLE.each {|row|
+  User.new(
+    username:row["username"],
+    password:row["password"],
+    role:row["role"]
+  ).save
+}
+
+QUIZZES_TABLE = CSV.read("../samples/quizzes.csv", converters: :numeric, headers: true)
+
+# QUESTIONS_TABLE = CSV.read("../samples/questions.csv", converters: :numeric, headers: true)
+
+# ANSWERS_TABLE = CSV.read("../samples/answers.csv", converters: :numeric, headers: true)

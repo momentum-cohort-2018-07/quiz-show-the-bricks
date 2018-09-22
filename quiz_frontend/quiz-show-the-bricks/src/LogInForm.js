71177548abfa20fc5f0c29169092f7cd5e8893a2
Dan Codae <drcoda@gmail.com>
2018-09-22 13:01:20 -0400

    begin writing of login code

---
+++ quiz_frontend/quiz-show-the-bricks/src/LogInForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class LogIn extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      username: '',
+      password: '',
+      errorMsg: null
+    }
+    this.handlesubmit = this.handlesubmit.bind(this)
+  }
+  render () {
+    return (
+      
+    )
+  }
+}
+
+export default LogIn

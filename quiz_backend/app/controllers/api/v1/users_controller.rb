04e1ae4bd620c490ca10cd627aa25a7c04e42710
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-09-22 20:27:43 -0400

    user controller and model updated to allow log in

--- quiz_backend/app/controllers/api/v1/users_controller.rb
+++ quiz_backend/app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,26 @@
 class Api::V1::UsersController < ApplicationController
+  
   def login
-    render :json => {
-      :api_token => "dummytoken123",
-      :username => "dummyusername",
-      :user_id => 1,
-      :role => "user"
-      }.to_json
+    
   end
 
   def create
-    render :json => {
-      :api_token => "dummytoken123",
-      :username => "dummyusername",
-      :user_id => 1,
-      :role => "user"
-      }.to_json
+    @user = User.new(user_params)
+
+    @user.save 
+
+    render json.user do
+        json.api_token @user.api_token
+        json.id @user.id
+        json.username @user.username
+        json.role @user.role
+    end
+  end
+  
+  private
+
+  def user_params
+    params.permit(:username, :password)
   end
 
 end

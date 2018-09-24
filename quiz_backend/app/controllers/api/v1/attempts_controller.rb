19f7989f6beb9c366a7e8f51a8e87aea59dbcff0
lifeprolific <thegregoman@gmail.com>
2018-09-24 16:35:28 -0400

    added score_quiz method

--- quiz_backend/app/controllers/api/v1/attempts_controller.rb
+++ quiz_backend/app/controllers/api/v1/attempts_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::AttemptsController < ApplicationController
 
   def create
+    if authenticated_user
+      @attempt = Attempt.new({
+        "quiz_id": params[:quiz][:id],
+        "user_id": authenticated_user.id
+      })
+      @attempt.score_quiz(params[:quiz])
+      if @attempt.save()
+        
+      else
+
+      end
+    else
+      puts "*************** not authenticated"
+    end
     
   end
+
+  private
 
 end

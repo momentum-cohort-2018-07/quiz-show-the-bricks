0302e997f6cd84bb3ce4712dd19e7e53a85e6223
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-09-23 22:09:42 -0400

    added view for quiz, question, answer

--- quiz_backend/app/controllers/api/v1/quizzes_controller.rb
+++ quiz_backend/app/controllers/api/v1/quizzes_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::QuizzesController < ApplicationController
+
   
   def index
     @quizzes = Quiz.all.order(updated_at: :desc)
   end
 
+  def show
+    if authenticated_user
+      # @quiz = Quiz.find(params[:id])
+      puts "**********************authentication succeeded"
+    else
+      puts "**********************authentication failed"
+    end
+  end
 end
--- quiz_backend/app/controllers/application_controller.rb
+++ quiz_backend/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    helper_method :authenticated_user
+
+    protected
+
+    def authenticated_user
+        true
+        # if ActionController::HttpAuthentication::Token.token_and_options(request)
+        #     User.find_by api_token: ActionController::HttpAuthentication::Token.token_and_options(request)[0]
+        # else
+        #     false
+        # end
+    end
+
 end

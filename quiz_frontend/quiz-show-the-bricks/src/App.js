47af6c7757d090ae912aecefc86b46e4adac8308
Dan Codae <drcoda@gmail.com>
2018-09-25 11:50:33 -0400

    add logout functionality

--- quiz_frontend/quiz-show-the-bricks/src/App.js
+++ quiz_frontend/quiz-show-the-bricks/src/App.js
@@ -4,7 +4,7 @@ import 'bulma/css/bulma.css'
 import EnterSite from './EnterSite'
 import QuizIndex from './QuizIndex'
 import './index.css'
-import Sidebar from './Sidebar'
+import Sidebar from './SideBar'
 
 // import {
 //   BrowserRouter as Router,
@@ -21,21 +21,22 @@ class App extends Component {
     const username = window.localStorage.getItem('username')
     const token = window.localStorage.getItem('token')
     if (username && token) {
-      // this.state.currentUser = { username, token }
+      this.state.currentUser = { username, token }
     // this.setState(state => ({token: 'token'})
     }
     this.setCurrentUser = this.setCurrentUser.bind(this)
-    // // this.logout = this.logout.bind(this)
+    this.logOut = this.logOut.bind(this)
   }
 
   setCurrentUser (user) {
+    console.log(user)
     window.localStorage.setItem('username', user.username)
     window.localStorage.setItem('token', user.token)
     this.setState({ currentUser: user })
   }
 
   logOut () {
-    window.setCurrentUser(null)
+    window.localStorage.setItem('username', null)
     window.localStorage.clear()
     this.setState({
       currentUser: null

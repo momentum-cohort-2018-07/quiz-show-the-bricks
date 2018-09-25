6c564792a2d69df969baf223b527766c42b4a7e7
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-09-25 16:49:01 -0400

    quizzes have an average and best score, user has a best score

--- quiz_backend/app/models/quiz.rb
+++ quiz_backend/app/models/quiz.rb
@@ -3,8 +3,22 @@ class Quiz < ApplicationRecord
     has_many :attempts
 
     def average_score
-        self.attempts.sum(:value) do 
-        value.sum / attempts.length
-        end
+        count = self.attempts.length 
+        scores = self.attempts.map { |x| x.score }
+        sum = scores.inject(0) {|sum, i|  sum + i }
+        sum / count.to_f
+    end
+
+    def best_score
+        scores = self.attempts.map { |x| x.score }
+        scores.max
+    end
+
+    def users_best_score(user)
+        attempts = Attempt.where({ user_id: user.id, quiz_id: self.id })
+        scores = attempts.map { |x| x.score }
+        scores.max
     end
 end
+
+
